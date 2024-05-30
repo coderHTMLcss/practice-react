4d9fde69c0636c870f759e5d95191662807eb10c
viacheslav <kabaniets1990@gmail.com>
2024-05-30 13:42:06 +0300

    add ligth-dark-mode component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Accordian from "./components/Accordian/Accordian";
 import Slider from "./components/Image-Slider/Slider";
 import LoadMoreData from "./components/Load-more-data/Load-more-data";
+import LigthDarkMode from "./components/Ligth-dark-mode/Liigth-dark-mode";
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
       {/* <Slider url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"} /> */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+      <LigthDarkMode />
     </div>
   );
 }
